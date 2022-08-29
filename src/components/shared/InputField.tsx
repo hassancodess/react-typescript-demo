5e6cb54d1c6fd473dbbbd5e18b62b236233b77b3
hassancodess <dev.hassancodes@gmail.com>
2022-08-29 23:21:39 +0500

    Add useRef & some events

--- src/components/shared/InputField.tsx
+++ src/components/shared/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 interface Props {
   todo: string
@@ -7,17 +7,29 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inp = useRef<HTMLInputElement>(null)
   return (
     <div className='form-control my-10'>
-      <div className='input-group justify-center'>
+      <div className='flex justify-center'>
         <input
+          ref={inp}
           type='text'
           placeholder='Enter something…'
-          className='input input-bordered w-96'
+          className='input input-bordered input-primary w-full max-w-lg'
           value={todo}
-          onChange={(e) => setTodo(e.target.value)}
+          onChange={(e) => {
+            setTodo(e.target.value)
+          }}
+          onKeyDown={(e) => {
+            console.log(typeof e.key)
+            if (e.key == 'Enter') {
+              console.log('Before Add')
+              handleAdd(e)
+              inp.current?.blur()
+            }
+          }}
         />
-        <button className='btn p-4' onClick={handleAdd}>
+        <button className='btn btn-primary p-4 mx-2' onClick={handleAdd}>
           Enter
         </button>
       </div>
